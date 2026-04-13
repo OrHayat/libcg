8201aa0f303bf11b7af2bd34baff25e2a290c3c9
Or Hayat <or.hayat@volumez.com>
2026-04-13 14:05:53 +0300

    add pixel buffer, fill with static color

--- src/platform/platform.h
+++ src/platform/platform.h
@@ -4,6 +4,12 @@
 #include <stdbool.h>
 #include <stdint.h>
 
+typedef struct {
+    uint32_t *pixels;
+    int       width;
+    int       height;
+} platform_framebuffer_t;
+
 /* Lifecycle */
 bool platform_init(int width, int height, const char *title);
 void platform_shutdown(void);
@@ -11,5 +17,8 @@ void platform_shutdown(void);
 /* Per-frame */
 void platform_poll_events(bool *quit_requested);
 void platform_present(void);
+
+/* Accessors */
+platform_framebuffer_t *platform_get_framebuffer(void);
 
 #endif /* PLATFORM_H */
