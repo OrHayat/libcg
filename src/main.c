25b32ec2e62f28e488c7f22d5760dbb55c0338af
OrHayat <or.hayat@volumez.com>
2026-04-20 08:58:21 +0300

    add debug patterns (1-4 keys) + RGB macros (#9)

--- src/main.c
+++ src/main.c
@@ -1,6 +1,45 @@
 #include "platform/platform.h"
 #include "render/framebuffer.h"
+#include "util/common.h"
+#include <math.h>
 #include <stdio.h>
+#include <stdlib.h>
+
+typedef enum {
+    PATTERN_SOLID = 0,
+    PATTERN_GRADIENT,
+    PATTERN_CYCLE,
+    PATTERN_NOISE,
+} pattern_t;
+
+static void render_solid(platform_framebuffer_t *fb) {
+    framebuffer_clear(fb, RGB(0xFF, 0x88, 0x00));
+}
+
+static void render_gradient(platform_framebuffer_t *fb) {
+    int w = fb->width > 1 ? fb->width - 1 : 1;
+    for (int y = 0; y < fb->height; y++) {
+        for (int x = 0; x < fb->width; x++) {
+            u8 r = (u8)((x * 255) / w);
+            fb->pixels[y * fb->width + x] = RGB(r, 0, 255 - r);
+        }
+    }
+}
+
+static void render_cycle(platform_framebuffer_t *fb, double t) {
+    u8 r = (u8)(128.0 + 127.0 * sin(t));
+    u8 g = (u8)(128.0 + 127.0 * sin(t + 2.094));
+    u8 b = (u8)(128.0 + 127.0 * sin(t + 4.188));
+    framebuffer_clear(fb, RGB(r, g, b));
+}
+
+static void render_noise(platform_framebuffer_t *fb) {
+    int n = fb->width * fb->height;
+    for (int i = 0; i < n; i++) {
+        u8 v = (u8)(rand() & 0xFF);
+        fb->pixels[i] = RGB(v, v, v);
+    }
+}
 
 int main(void) {
     if (!platform_init(1280, 720, "libcg")) {
@@ -10,6 +49,8 @@ int main(void) {
 
     bool print_mouse_coords = false;
     int  last_mx = -1, last_my = -1;
+    pattern_t pattern = PATTERN_SOLID;
+    int  frame_count = 0;
 
     platform_input_t input = {0};
     while (!input.quit_requested) {
@@ -25,6 +66,10 @@ int main(void) {
             platform_toggle_fullscreen();
         if (platform_is_key_pressed(&input, PLATFORM_KEY_ESCAPE) && platform_is_fullscreen())
             platform_toggle_fullscreen();
+        if (platform_is_key_pressed(&input, PLATFORM_KEY_1)) pattern = PATTERN_SOLID;
+        if (platform_is_key_pressed(&input, PLATFORM_KEY_2)) pattern = PATTERN_GRADIENT;
+        if (platform_is_key_pressed(&input, PLATFORM_KEY_3)) pattern = PATTERN_CYCLE;
+        if (platform_is_key_pressed(&input, PLATFORM_KEY_4)) pattern = PATTERN_NOISE;
 
         if (print_mouse_coords && (input.mouse.x != last_mx || input.mouse.y != last_my)) {
             printf("mouse: (%d, %d)\n", input.mouse.x, input.mouse.y);
@@ -48,8 +93,14 @@ int main(void) {
             printf("scroll: dx=%.1f dy=%.1f\n", (double)input.mouse.scroll_dx, (double)input.mouse.scroll_dy);
 
         platform_framebuffer_t *fb = platform_get_framebuffer();
-        framebuffer_clear(fb, 0xFFFF8800);  /* AARRGGBB - solid orange */
+        switch (pattern) {
+            case PATTERN_SOLID:    render_solid(fb); break;
+            case PATTERN_GRADIENT: render_gradient(fb); break;
+            case PATTERN_CYCLE:    render_cycle(fb, frame_count * 0.05); break;
+            case PATTERN_NOISE:    render_noise(fb); break;
+        }
 
+        frame_count++;
         platform_present();
     }
 
