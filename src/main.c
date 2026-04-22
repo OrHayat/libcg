7abd1075069be1037d589c6e2005e79e7508aea2
OrHayat <or.hayat@volumez.com>
2026-04-23 02:26:21 +0300

    add T key framebuffer info (#12)

--- src/main.c
+++ src/main.c
@@ -207,6 +207,10 @@ int main(void) {
                 bg_checkerboard = !bg_checkerboard;
                 printf("background: %s\n", bg_checkerboard ? "checkerboard" : "transparent");
             }
+            if (platform_is_key_pressed(&input, PLATFORM_KEY_T)) {
+                platform_framebuffer_t *fbi = platform_get_framebuffer();
+                printf("framebuffer: %dx%d\n", fbi->width, fbi->height);
+            }
         }
 
         /* Mouse output is suppressed while typing a color */
